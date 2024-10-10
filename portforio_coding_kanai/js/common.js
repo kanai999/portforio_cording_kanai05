5de261561c6ee9677e84afaee2b465b7735ad448
kanai999 <k.kanai.926@gmail.com>
2024-10-10 13:57:33 +0900

    ポートフォリオ

--- portforio_coding_kanai/js/common.js
+++ portforio_coding_kanai/js/common.js
@@ -2,3 +2,23 @@ $('.hamburger,.sp-nav a').on('click', function () {
     $('.sp-nav').fadeToggle();
     $('.hamburger').toggleClass('open');
 });
+
+$(function () {
+    // スクロールしたら「トップに戻る」ボタンが表示される
+    const toTopButton = $(".js-to-top");
+    const scrollHeight = 100;
+    toTopButton.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            toTopButton.fadeIn();
+        } else {
+            toTopButton.fadeOut();
+        }
+    });
+
+    // 「トップに戻る」ボタンをクリックしたらスクロールで戻る
+    toTopButton.click(function () {
+        $("body,html").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+});
